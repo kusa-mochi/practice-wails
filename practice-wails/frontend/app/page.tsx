68900c33e2b906bcefa2521322bb6030d230c2b6
Kusa Mochi <whoatemyapplepie@gmail.com>
2026-07-05 00:17:16 +0900

    バックエンドからの応答を画面に表示するテストを追加

--- practice-wails/frontend/app/page.tsx
+++ practice-wails/frontend/app/page.tsx
@@ -1,6 +1,16 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { Greet } from "../wailsjs/go/main/App";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [greetResponse, setGreetResponse] = useState<string | null>(null);
+  useEffect(() => {
+    // バックエンドのGreetメソッドを呼び出す。
+    Greet("mokemoke").then((response) => {
+      setGreetResponse(response);
+    });
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -27,6 +37,8 @@ export default function Home() {
           </a>
         </div>
       </div>
+
+      <div>res:{greetResponse}</div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
